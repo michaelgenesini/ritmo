c1367bad6a0eb7b07a16038539f04b873079f958
Michael Genesini <michael.genesini@cabify.com>
2025-03-22 20:45:08 +0100

    Remove unused import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef, useCallback } from "react";
+import { useState, useEffect, useRef } from "react";
 import { loadRhythms, Rhythm } from "@/utils/loadRhythms";
 import { loadSounds } from "@/utils/loadSounds";
 import { Button } from "@/components/Button";
